d15375dc9b8b5d6f8eec8135c9486d6d0c59c8d6
Vikas Kapparad <vikaskapparad@gmail.com>
2025-04-20 16:37:04 +0530

    docs: added multiple folders of HTML-CSS works

---
+++ project-navbar/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger-menu'); 
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
